3e282346be38512f18c21884e5b1f0a27988054e
johnjonhjoaojuan <johnaguiar2000@hotmail.com>
2025-02-11 12:24:49 +0000

    Javascript click-through album

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,14 @@ const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
     const mySrc = myImage.getAttribute("src");
-    if (mySrc === "https://johnjonhjoaojuan.github.io/scrap/images/prg1.png") {
-        myImage.setAttribute("src", "https://johnjonhjoaojuan.github.io/scrap/images/prg2.png");
-    } else {
-        myImage.setAttribute("src", "https://johnjonhjoaojuan.github.io/scrap/images/prg1.png");
-    }
-});
+    if (mySrc === "images/prg1.png") {
+        myImage.setAttribute("src", "images/prg2.png");
+    } else { if (mySrc === "images/prg2.png"){
+                myImage.setAttribute("src", "images/prg3.png");
+            } else { if (mySrc === "images/prg3.png") {
+                        myImage.setAttribute("src", "images/prg4.png");
+                    } else { myImage.setAttribute("src", "images/prg1.png")}
+                    }
+                }
+            }
+);
